c269a6e08c6e6412efd70fe358ccedaa35645e4e
nicodmp <nicolasdmpr@gmail.com>
2025-06-06 23:39:11 -0300

    fix: update app module for docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,25 +5,23 @@ import { VendedoresModule } from './vendedores/vendedores.module';
 
 @Module({
   imports: [
-    // Carrega .env
     ConfigModule.forRoot({ isGlobal: true }),
-    // Configura TypeORM usando vars de ambiente
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
         type: 'postgres',
-        host: config.get('DB_HOST'),
-        port: +config.get<number>('DB_PORT'),
-        username: config.get('DB_USER'),
-        password: config.get('DB_PASSWORD'),
-        database: config.get('DB_NAME'),
+        host: config.get<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_NAME'),
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: true, // Em dev: cria/atualiza tabelas automaticamente
+        synchronize: true,
+        autoLoadEntities: true,
       }),
     }),
     VendedoresModule,
-    // Outros modules serão importados aqui...
   ],
 })
 export class AppModule {}
